aa6811717daedd4f9f6087d2dc1572566e0b59e0
Dreamer899 <je626883@gmail.com>
2024-10-05 12:48:43 -0300

    Create hello_win.c

---
+++ C_/hello_win.c
@@ -0,0 +1,10 @@
+// Hello World Using MessageBox Function from windows.h header file
+#include <windows.h>
+#include <stdio.h>
+
+int main() {
+
+    MessageBox(NULL,"Hello World","Hello World",MB_OK);
+
+    return 0;
+}
